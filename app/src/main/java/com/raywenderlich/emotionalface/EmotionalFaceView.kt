d93385c79c9aeef350b3a39d0a646f6708edfc65
Aung Thiha <aung@appsynth.net>
2019-12-24 15:33:11 +0700

    draw facebackground

---
+++ app/src/main/java/com/raywenderlich/emotionalface/EmotionalFaceView.kt
@@ -0,0 +1,62 @@
+package com.raywenderlich.emotionalface
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class EmotionalFaceView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    // Paint object for coloring and styling
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    // Some colors for the face background, eyes and mouth.
+    private var faceColor = Color.YELLOW
+    private var eyesColor = Color.BLACK
+    private var mouthColor = Color.BLACK
+    private var borderColor = Color.BLACK
+    // Face border width in pixels
+    private var borderWidth = 4.0f
+    // View size in pixels
+    private var size = 320
+
+    override fun onDraw(canvas: Canvas) {
+        // call the super method to keep any drawing from the parent side.
+        super.onDraw(canvas)
+
+        drawFaceBackground(canvas)
+        drawEyes(canvas)
+        drawMouth(canvas)
+    }
+
+
+    private fun drawFaceBackground(canvas: Canvas) {
+        // 1
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        // 2
+        val radius = size / 2f
+
+        // 3
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        // 4
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+
+        // 5
+        canvas.drawCircle(size / 2f, size / 2f, radius - borderWidth / 2f, paint)
+    }
+
+    private fun drawEyes(canvas: Canvas) {
+    }
+
+    private fun drawMouth(canvas: Canvas) {
+    }
+
+}
